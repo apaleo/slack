d82743ef11d4e376acb1703fa6e86c4ed08450cf
Eugene Lahansky <e.lahanskiy@gmail.com>
2021-02-28 13:42:14 +0100

    Direct install URL (#27)
    * Direct install endpoint with redirect to slack oauth
    
    * Move 302 status to constants

--- src/app/routers/index.ts
+++ src/app/routers/index.ts
@@ -1,13 +1,18 @@
 import { Router } from 'express';
+import { httpTempRedirectStatusCode } from 'src/utils/static';
 import * as apaleoOAuth from './apaleo-oauth';
 import * as apaleoWebhook from './apaleo-webhook';
 
-export const enableRoutes = (router: Router) => {
+export const enableRoutes = (router: Router, installUrl: string) => {
   apaleoOAuth.enableRoutes(router);
 
   apaleoWebhook.enableRoutes(router);
 
+  router.get('/direct-install', (_, res) => {
+    return res.redirect(httpTempRedirectStatusCode, installUrl);
+  });
+
   router.get('/health', (_, res) => {
-    res.send('OK');
+    return res.send('OK');
   });
 };
