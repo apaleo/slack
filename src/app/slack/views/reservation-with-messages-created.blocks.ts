cfad9582ae06002254ddbc199ce52e32777f9535
Eugene Lahansky <e.lahanskiy@gmail.com>
2021-05-14 17:54:24 +0200

    Allow to receive all reservation created events

--- src/app/slack/views/reservation-with-messages-created.blocks.ts
+++ src/app/slack/views/reservation-with-messages-created.blocks.ts
@@ -1,22 +1,32 @@
 import { ReservationModel } from 'clients/booking/models';
+import { isEmpty } from 'lodash';
 import { apaleoUIBaseUrl } from 'src/settings';
 import { Markup } from './common.blocks';
 import { MarkupBuilder } from './markup-builder';
 
-export const reservationWithMessagesCreatedMarkup = (
+export const reservationCreatedMarkup = (
   reservation: ReservationModel
 ): Markup => {
-  return new MarkupBuilder()
-    .addContextBlock(
-      ':warning: Reservation was created with validation messages :warning:\n'
-    )
-    .addSectionBlock(
-      (reservation?.validationMessages || []).reduce((result, msg) => {
-        result += `_*${msg.message}*_\n`;
+  let builder = new MarkupBuilder();
+  if (!isEmpty(reservation.validationMessages)) {
+    builder = builder
+      .addContextBlock(
+        ':warning: Reservation was created with validation messages :warning:\n'
+      )
+      .addSectionBlock(
+        (reservation?.validationMessages || []).reduce((result, msg) => {
+          result += `_*${msg.message}*_\n`;
 
-        return result;
-      }, '')
-    )
+          return result;
+        }, '')
+      );
+  } else {
+    builder = builder.addContextBlock(
+      ':information_source: Reservation was created :information_source:\n'
+    );
+  }
+
+  return builder
     .addSectionBlock(
       `*<${apaleoUIBaseUrl}/${reservation.property.id}/reservations/${reservation.id}/actions?source=slack|${reservation.id}>*\n` +
         `_${reservation.primaryGuest?.lastName}_\n`
